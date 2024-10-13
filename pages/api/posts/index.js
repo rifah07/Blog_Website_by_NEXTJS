9789a6279f0bfa6769e8cd5a80a80b7df0b271e9
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-13 23:07:01 +0600

    page to display posts- completed

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -3,17 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    if(req.method === 'GET'){
-        try{
-            const posts = await prisma.post.findMany();
-            res.status(200).json(posts);
-        }catch(error){
-            res.status(500).json({error : 'Failed to fetch posts'});
-        }finally{
-            await prisma.$disconnect();
-        }
-    }    else{
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end('Method not allowed');
+  if (req.method === "GET") {
+    try {
+      const posts = await prisma.post.findMany();
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch posts" });
+    } finally {
+      await prisma.$disconnect();
     }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end("Method not allowed");
+  }
 }
