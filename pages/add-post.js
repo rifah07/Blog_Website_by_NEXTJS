211eb99ff47ed254a6ddaa3a8b3e7884845925bc
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-14 20:10:50 +0600

    add-post implemented

---
+++ pages/add-post.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function AddPostPage() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+
+      if (!response) {
+        throw new Error("Something went wrong while creating the new post");
+      }
+
+      router.push("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4 bg-gray-200">
+      <h1 className="text-2xl text-blue-950 font-bold mb-4">Add a New Post</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label
+            htmlFor="title"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+            className="mt-1 px-2 block w-full rounded-md border-gray-300 text-blue-950 shadow-sm"
+          ></input>
+        </div>
+        <div>
+          <label
+            htmlFor="content"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Content
+          </label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+            className="mt-1 px-2 block w-full rounded-md border-gray-300 text-blue-950 shadow-sm"
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-500 text-white rounded-md"
+            disabled={loading}
+          >
+            {loading ? "Creating..." : "Create post"}
+          </button>
+        </div>
+        {error && <p className="text-red-500">{error}</p>}
+      </form>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import '../app/globals.css';
+import "../app/globals.css";
+import Link from "next/link";
 
 export async function getServerSideProps() {
   try {
@@ -19,15 +20,24 @@ export async function getServerSideProps() {
 export default function PostPage(props) {
   return (
     <div className="container mx-auto px-4 py-8 bg-gray-50 ">
-      <h1 className="text-4xl font-bold text-center mb-8 text-black">Blog Posts</h1>
+      <h1 className="text-4xl font-bold text-center mb-8 text-black">
+        Blog Posts
+      </h1>
+      <Link href={"/add-post"} legacyBehavior> 
+        <a className="inline-block mb-4 px-4 py-2 bg-green-500 text-white rounded-md">
+          Add a Post
+        </a>
+      </Link>
       <ul className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-        {props.posts.map((post) => (
+      {props.posts.map((post) => (
           <li
             key={post.id}
             className="bg-white shadow rounded-lg overflow-hidden"
           >
             <div className="p-6 ">
-              <h2 className="text-2xl text-black font-semibold mb-4">{post.title}</h2>
+              <h2 className="text-2xl text-black font-semibold mb-4">
+                {post.title}
+              </h2>
               <p className="text-gray-700 mb-4">{post.content}</p>
               <small className="text-gray-500">
                 {new Date(post.date).toLocaleDateString("en-US", {
