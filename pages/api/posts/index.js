7c8d05ab382fe150f27e8136bea31afc35659734
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-10-14 17:09:31 +0600

    API to ADD post cooaded

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -12,8 +12,24 @@ export default async function handler(req, res) {
     } finally {
       await prisma.$disconnect();
     }
-  } else {
-    res.setHeader("Allow", ["GET"]);
+  } else if(req.method === "POST"){
+    const { title, content } = req.body;
+    try{
+      const newPost= await prisma.post.create({
+        data:{
+          title,
+          content
+        }
+      });
+
+      res.status(201).json(newPost);
+    } catch(error){
+      console.error('Error creating the post: ',error);
+      res.status(500).json({message: 'Error creating the post'});
+    }
+  }
+  else {
+    res.setHeader("Allow", ['GET', 'POST']);
     res.status(405).end("Method not allowed");
   }
 }
